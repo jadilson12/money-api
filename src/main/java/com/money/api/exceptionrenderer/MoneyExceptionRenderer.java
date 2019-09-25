d5384777f1e3feaa952af21b98ad8184a5801bee
Jadilson Guedes <jadilson12@gmail.com>
2019-09-25 10:11:09 -0300

    [ADD] Validando  valores invalidos com Bean Validation

--- src/main/java/com/money/api/exceptionrenderer/MoneyExceptionRenderer.java
+++ src/main/java/com/money/api/exceptionrenderer/MoneyExceptionRenderer.java
@@ -3,15 +3,24 @@ package com.money.api.exceptionrenderer;
 import net.bytebuddy.build.Plugin;
 import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @ControllerAdvice
 public class MoneyExceptionRenderer extends ResponseEntityExceptionHandler {
@@ -21,10 +30,31 @@ public class MoneyExceptionRenderer extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-          HttpHeaders headers, HttpStatus status, WebRequest request) {
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+
         String messageUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
         String messageDesenvolvedor = ex.getCause().toString();
-        return handleExceptionInternal(ex, new Erro(messageUsuario, messageDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+        List<Erro>  erros = Arrays.asList(new Erro(messageUsuario, messageDesenvolvedor));
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Erro> erros = criarListaDeErro(ex.getBindingResult());
+        return handleExceptionInternal(ex,erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criarListaDeErro(BindingResult bindingResult) {
+        List<Erro> erros = new ArrayList<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String mensagemDesenvolvedor = fieldError.toString();
+            erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        }
+
+        return erros;
     }
 
     public static class Erro {
--- src/main/java/com/money/api/resource/CategoriaResource.java
+++ src/main/java/com/money/api/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -27,7 +28,7 @@ public class CategoriaResource {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+    public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva =  categoriaRepository.save(categoria);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
