8bcb2bdc209993fed40feaba78e9b4eb3aa3d267
Jadilson Guedes <jadilson12@gmail.com>
2019-09-25 08:50:11 -0300

    [ADD] Add o create da categoria e nega atributos desconhecido

--- src/main/java/com/money/api/resource/CategoriaResource.java
+++ src/main/java/com/money/api/resource/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.money.api.resource;
 import com.money.api.model.Categoria;
 import com.money.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -17,7 +21,29 @@ public class CategoriaResource {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public List<Categoria> lista(){
+    public List<Categoria> lista() {
         return  categoriaRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva =  categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("location", uri.toASCIIString());
+
+        return ResponseEntity.created((uri)).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarByCodigo (@PathVariable  Long codigo) {
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+        if (categoria.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return  ResponseEntity.ok(categoria.get());
+    }
+
 }
